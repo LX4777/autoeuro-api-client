9b0babfe50cddd86497f57acfc9de5bcd0d6694e
Vasiliy Rozhkov <profilrv@gmail.com>
2024-08-25 14:46:08 +0500

    feat(requestData): добавление типа SearchItemsRequestData

--- src/RequestData.ts
+++ src/RequestData.ts
@@ -5,3 +5,11 @@ export type GetWarehousesRequestData = {
 export type SearchBrandsRequestData = {
   code: string;
 }
+
+export type SearchItemsRequestData = {
+  brand: string;
+  code: string;
+  delivery_key: string;
+  with_crosses: 0|1;
+  with_offers: 0|1;
+}
