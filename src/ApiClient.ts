c8912568cad1de1b8bc4eacd8116d7b7b478fc53
Vasiliy Rozhkov <profilrv@gmail.com>
2024-08-25 12:09:10 +0500

    refactor(ApiClient)

--- src/ApiClient.ts
+++ src/ApiClient.ts
@@ -1,7 +1,7 @@
 import { ApiClientConfig } from './Contracts/ApiClientConfig.js';
 import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 
-class ApiClient {
+export class ApiClient {
   protected axiosInstance: AxiosInstance;
 
   constructor(config: ApiClientConfig) {
@@ -12,30 +12,7 @@ class ApiClient {
     });
   }
 
-  protected async request<T>(method: string, endpoint: string, data?: any): Promise<T> {
-    const config: AxiosRequestConfig = {
-      method,
-      url: endpoint,
-      data,
-    };
-
-    const response: AxiosResponse<T> = await this.axiosInstance.request<T>(config);
-    return response.data;
-  }
-
-  protected get<T>(endpoint: string, params?: Record<string, string>): Promise<T> {
-    return this.axiosInstance.get<T>(endpoint, { params }).then(response => response.data);
-  }
-
-  protected post<T>(endpoint: string, data: any): Promise<T> {
-    return this.axiosInstance.post<T>(endpoint, data).then(response => response.data);
-  }
-
-  protected put<T>(endpoint: string, data: any): Promise<T> {
-    return this.axiosInstance.put<T>(endpoint, data).then(response => response.data);
-  }
-
-  protected delete<T>(endpoint: string): Promise<T> {
-    return this.axiosInstance.delete<T>(endpoint).then(response => response.data);
+  protected async request<T>(endpoint: string, data?: any): Promise<AxiosResponse<T>> {
+    return this.axiosInstance.post<T>(endpoint, data);
   }
 }
