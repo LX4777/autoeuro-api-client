f1827aba74e7e0f7b2b2e81f54d9e3a02b33426c
Vasiliy Rozhkov <profilrv@gmail.com>
2024-08-26 14:36:19 +0500

    refactor(example): изменение примера, так как целевую версию js понизил

--- src/examples/index.ts
+++ src/examples/index.ts
@@ -1,10 +1,14 @@
 import { AutoeuroService } from '../AutoeuroService.js';
 
-const token = "YOUR TOKEN HAS TO BE HERE";
+const token = 'YOUR TOKEN HAS TO BE HERE';
 
 const client = new AutoeuroService({
   baseURL: 'https://api.autoeuro.ru/api/v2/json',
   token: token,
 });
 
-console.log(await client.getStatuses());
+const example = async () => {
+  console.log(await client.getStatuses());
+};
+
+example()
